6884078e2380f56bb3e43a6065fdee62e7e3669f
Ches <chesy47@gmail.com>
2021-11-03 11:55:48 +0200

    navbar --done

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+	//Menu
+	function toggleMenu() {
+		$('.menu-toggle').toggleClass('menu-toggle_active');
+		$('.top-menu').toggleClass('top-menu_active');
+	}
 
-	// $('body').hide()
+	$('.menu-toggle').click(function() { toggleMenu() })
+
+	function closeMenu() {
+		$('.menu-toggle').removeClass('menu-toggle_active');
+		$('.top-menu').removeClass('top-menu_active');
+	}
+
+	$(document).click(function(e) {
+		if($(e.target).closest('.menu-container').length) return
+		closeMenu();
+	})
 
 })
