19f4afb7049193e6dbf3bb28fe5bc2d645d0f0d5
Ches <chesy47@gmail.com>
2021-12-01 17:15:07 +0200

    home-slider --Done

--- js/common.js
+++ js/common.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+	$('.home-slider__heading').each(function() {
+		let text = $(this).text().split(' '),
+			first = text.shift()
+		$(this).html(`${first} <br><span>${text.join(' ')}</span>`)	
+	})
+
 	//Menu
 	function toggleMenu() {
 		$('.menu-toggle').toggleClass('menu-toggle_active');
@@ -22,6 +29,28 @@ document.addEventListener('DOMContentLoaded', function() {
 		speed: 800,
 		effect: 'fade',
 		centeredSlides: true,
+		pagination: {
+			el: '.home-slider__pagination',
+			type: 'custom',
+			renderCustom: function(swiper, current, total) {
+				let indexTotal = total >= 10 ? total : `0${total}`
+				let indexCurrent = current >= 10 ? total : `0${current}`
+				return `<b>${indexCurrent}</b><span></span> ${indexTotal}`
+			}
+		},
+		scrollbar: {
+			el: '.home-slider__scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl: '.home-slider__prev',
+			nextEl: '.home-slider__next'
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false
+		},
+		runCallbacksOnInit: true
 	})
 
 })
