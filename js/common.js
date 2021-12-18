8c2f7d2e941f7ce4b71ae70f4df01d9324b065fe
Ches <chesy47@gmail.com>
2021-12-18 16:30:06 +0200

    home-callback --Done

--- js/common.js
+++ js/common.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+	MicroModal.init({
+		openTrigger: "data-custom-open",
+		closeTrigger: "data-custom-close",
+		disableScroll: true,
+		disableFocus: true,
+		awaitOpenAnimation: true,
+		awaitCloseAnimation: true,
+	});
+
+	$('[data-custom-open]').click(function() {
+		$('.modal [name=form]').val($(this).data('form'))
+	});
+
+	$('[data-custom-close]').click(function() {
+		$('.modal [name=form]').val('')
+	});
+
+
 	$('.home-slider__heading').each(function() {
 		let text = $(this).text().split(' '),
 			first = text.shift()
